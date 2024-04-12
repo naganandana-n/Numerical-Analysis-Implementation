c0b83b90c8c0c3519a5a5c42dcc71cb72558a9f3
naganandana <naganandana17@gmail.com>
2024-04-12 16:25:32 +0400

    commit

---
+++ Newtons-Method.py
@@ -0,0 +1,15 @@
+def newtonMethod(func, funcderiv, x, n):
+
+    def f(x):
+        f = eval(func)
+        return f
+    def df(x):
+        df = eval(funcderiv)
+        return df
+    
+    for intercept in range(1, n):
+        i = x - (f(x)/df(x))
+        x = i
+    print(f"The root was found to be at {x} after {n} iterations")
+
+newtonMethod("x**2 - 2", "2*x", 2, 10)
